9eddb212d16fac0d11055da673dce01162641e80
PaulMllr <lincore@ya.ru>
2018-12-02 19:44:03 +1000

    Add Bottom-Up mergesort implementation

--- src/MergeSortLauncher.java
+++ src/MergeSortLauncher.java
@@ -1,6 +1,7 @@
 import mergesort.MergeSort;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by Pavel Laktiushkin on 28.11.2018
@@ -21,10 +22,35 @@ public class MergeSortLauncher {
         System.out.println("Result: " + Arrays.toString(result));
 
 
-        // Part 2. Checking the mergesort algorithm end-to-end
-        Comparable[] unsortedArray = {'j', 'c', 'a', 'z', 'b', 'h', 'p', 'a','x'};
+        // Part 2. Checking the merge sort algorithm end-to-end
+        Comparable[] unsortedArray = {'c', 'a', 'g', 'd', 'b', 'h', 'p', 'a', 'x'};
         System.out.println("Unsorted array: " + Arrays.toString(unsortedArray));
         System.out.println("Sorted array: " + Arrays.toString(MergeSort.sort(unsortedArray)));
+
+        // Part 2.5. Checking the bottom up merge sort end-to-end
+        System.out.println("Bottom up sorted array: " + Arrays.toString(MergeSort.bottomUpMergeSort(unsortedArray)));
+
+
+        // Part 3. Performance testing
+        System.out.println("Performance test:");
+        Comparable[] hugeUnsortedArray = new Comparable[10000000];
+        Random random = new Random();
+        for (int i = 0; i < hugeUnsortedArray.length; i++) {
+            hugeUnsortedArray[i] = random.nextInt();
+        }
+
+        // Profiling recursive merge-sort
+        long before = System.currentTimeMillis();
+        MergeSort.sort(hugeUnsortedArray);
+        long after = System.currentTimeMillis();
+        System.out.printf("Merge sort for array[%d] took %dms%n", hugeUnsortedArray.length, after - before);
+
+        // Profiling Bottom-Up merge-sort
+        before = System.currentTimeMillis();
+        MergeSort.bottomUpMergeSort(hugeUnsortedArray);
+        after = System.currentTimeMillis();
+        System.out.printf("Bottom-Up merge sort for array[%d] took %dms%n", hugeUnsortedArray.length, after - before);
+
 
     }
 }
--- src/mergesort/MergeSort.java
+++ src/mergesort/MergeSort.java
@@ -2,6 +2,8 @@ package mergesort;
 
 import java.util.Arrays;
 
+import static java.lang.Math.min;
+
 /**
  * Created by Pavel Laktiushkin on 28.11.2018
  */
@@ -39,6 +41,46 @@ public class MergeSort {
         return result;
     }
 
+    /**
+     *
+     * @param array original array
+     * @param copyArray result array
+     * @param startIndex index of the beginning of the 1st merging part
+     * @param endIndex index of the end of the 2nd merging part
+     */
+    public static void merge(Comparable[] array, Comparable[] copyArray, int startIndex, int endIndex) {
+
+        // If there is nothing to merge - return
+        if (startIndex == endIndex) {
+            return;
+        }
+        // Calculating the border berween 2 parts
+        int middle = startIndex + 1 + (endIndex - startIndex) / 2;
+
+        int currentLeft = startIndex;
+        int currentRight = middle;
+
+        int leftLimit = middle;
+        int rightLimit = endIndex + 1;
+
+
+        // Iterating only through nodes we need to merge
+        for (int i = startIndex; i <= endIndex; i++) {
+
+            // If the 2nd part is exhausted, or array is out of bounds
+            if (currentRight >= rightLimit || currentRight > array.length - 1) {
+                copyArray[i] = array[currentLeft++];
+                continue;
+            }
+            // If the 1st part is exhaused, or the 1st part is greater the 2nd part
+            if (currentLeft >= leftLimit || array[currentLeft].compareTo(array[currentRight]) > 0) {
+                copyArray[i] = array[currentRight++];
+            } else {
+                copyArray[i] = array[currentLeft++];
+            }
+        }
+    }
+
     public static Comparable[] sort(Comparable[] source) {
         // Recursion limit. There is nothing to sort in the array
         if (source.length < 2) {
@@ -53,4 +95,31 @@ public class MergeSort {
         // Return the merged sorted result
         return merge(left, right);
     }
+
+    public static Comparable[] bottomUpMergeSort(Comparable[] source) {
+
+        // Copy data to the result array in advance
+        Comparable[] result = Arrays.copyOf(source, source.length);
+
+        // Merge part size. Starting from the bottom - 1. Then 2, 4, 8, etc
+        int partSize = 1;
+
+        while (partSize <= source.length / 2) {
+            for (int i = 0; i < source.length; i = i + 2 * partSize) {
+                int p1start = i;
+                int p2start = i + partSize;
+                // The array may end earlier than the part 2, so we need to choose min
+                int p2end = min(p2start + partSize - 1, source.length - 1);
+                // Merge consequently 2 parts
+                merge(source, result, p1start, p2end);
+            }
+            partSize += partSize;
+            // Before processing with the doule part size, we need to copy result array data to the source array
+            source = Arrays.copyOf(result, result.length);
+
+        }
+        return result;
+    }
+
+
 }
